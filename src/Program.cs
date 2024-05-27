31860f3b213730756d41046e2031ae2f3044fb7c
Turki Dhawi <T.Dhawi@outlook.com>
2024-05-27 20:11:47 +0300

    change the url

--- src/Program.cs
+++ src/Program.cs
@@ -128,5 +128,5 @@ app.UseHttpsRedirection();
 app.MapControllers();
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5125";
-// app.Run($"http://localhost:5343");
-app.Run($"http://localhost:{port}");
+// app.Run($"http://localhost:5125");
+app.Run($"http://0.0.0.0:{port}");
